70d28ef242e1bd60478b6c6e5eb6809f1203c643
mjk0513 <kirie0513@gmail.com>
2017-10-08 08:03:43 +0900

    OGP image added

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Scratch2CSV</title>
+	<title>CSV generator</title>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="./style/style.css" style="text/css">
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+	<!--OGPsettings-->
+	<meta property="og:title" content="CSV generator for Scratch Account">
+    <meta property="og:type" content="article">
+    <meta property="og:url" content="http://kirie.me/apps/scratch-csv">
+    <meta property="og:image" content="http://kirie.me/apps/scratch-csv/images/ogp.png">
+    <meta property="og:site_name"  content="CSV generator for Scratch Account">
+    <meta property="og:description" content="Create csv file for create new scratch student account.">
+    
 
 </head>
 
@@ -19,9 +28,11 @@
 		<div id="main">
 
 			<form action="makeCSV.php" method="POST">
-				<input type="number" name="numberOfPeople" placeholder="Number of create new accounts." class="textBox" min="1"><br>
-				<input type="text" name="username" placeholder="Same username." class="textBox"><br>
-				<input type="text" name="password" placeholder="Same password." class="textBox">
+				<input type="number" name="numberOfPeople" placeholder="Number of create new accounts." class="textBox" min="1" required><br>
+				<p>User name is ○○○○01, ○○○○02, ○○○○03 ... </p>
+				<input type="text" name="username" placeholder="Same username." class="textBox" required><br>
+				<p>Password is ○○○○01, ○○○○02, ○○○○03 ... </p>
+				<input type="text" name="password" placeholder="Same password." class="textBox" required>
 				<input type="hidden" name="flag" value="true">
 				<input type="submit" value="Create csv file" class="submitButton">
 			</form>
@@ -29,15 +40,19 @@
 		</div>
 
 		<div id="footer">
-			<p>Scratch is created by MIT MediaLab Life Long Kindergarten Team.</p>
+			<p><a href="https://scratch.mit.edu" class="textLink">Scratch</a> is created by MIT MediaLab Life Long Kindergarten Team.</p>
 			<p>This project is created by Kirie Miyajima.</p>
-			<p>Source is available on <a href="https://github.com/mjk0513/csvGenerator4ScratchAccount">GitHub</a>.</p>
+			<p>Source is available on <a href="https://github.com/mjk0513/csvGenerator4ScratchAccount" class="textLink">GitHub</a>.</p>
+		
+			<a href="http://kirie.me/"><i class="fa fa-user fa-3x"></i></a>
+			<a href="https://facebook.com/kiriemiyajima"><i class="fa fa-facebook-square fa-3x"></i></a> 
+			<a href="https://twitter.com/mjk_0513"><i class="fa fa-twitter-square fa-3x"></i></a> 
+			<a href="https://github.com/mjk0513"><i class="fa fa-github fa-3x"></i></a>
+
 		</div>
 
 		
-		<a href="https://facebook.com/kiriemiyajima"><i class="fa fa-facebook-square fa-3x"></i></a> 
-		<a href="https://twitter.com/mjk_0513"><i class="fa fa-twitter-square fa-3x"></i></a> 
-		<a href="https://github.com/mjk0513"><i class="fa fa-github fa-3x"></i></a>
+		
 
 
 	</div>

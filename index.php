f271798e082fbc2a55c0b682a7ec4707114d258d
mjk0513 <kirie0513@gmail.com>
2017-10-09 12:10:57 +0900

    アカウント作成最大数を50に設定

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="./style/style.css" style="text/css">
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="./style/animation.css" style="text/css">
 
 	<!--OGPsettings-->
 	<meta property="og:title" content="CSV generator for Scratch Account">
@@ -28,7 +29,7 @@
 		<div id="main">
 
 			<form action="makeCSV.php" method="POST">
-				<input type="number" name="numberOfPeople" placeholder="Number of create new accounts." class="textBox" min="1" required><br>
+				<input type="number" name="numberOfPeople" placeholder="Number of create new accounts." class="textBox" min="1" max="50" required><br>
 				<p>User name is ○○○○01, ○○○○02, ○○○○03 ... </p>
 				<input type="text" name="username" placeholder="Same username." class="textBox" required><br>
 				<p>Password is ○○○○01, ○○○○02, ○○○○03 ... </p>
@@ -44,10 +45,10 @@
 			<p>This project is created by Kirie Miyajima.</p>
 			<p>Source is available on <a href="https://github.com/mjk0513/csvGenerator4ScratchAccount" class="textLink">GitHub</a>.</p>
 		
-			<a href="http://kirie.me/"><i class="fa fa-user fa-3x"></i></a>
-			<a href="https://facebook.com/kiriemiyajima"><i class="fa fa-facebook-square fa-3x"></i></a> 
-			<a href="https://twitter.com/mjk_0513"><i class="fa fa-twitter-square fa-3x"></i></a> 
-			<a href="https://github.com/mjk0513"><i class="fa fa-github fa-3x"></i></a>
+			<a href="http://kirie.me/"><i class="fa fa-user fa-3x faa-spin animated-hover"></i></a>
+			<a href="https://facebook.com/kiriemiyajima"><i class="fa fa-facebook-square fa-3x faa-spin animated-hover"></i></a> 
+			<a href="https://twitter.com/mjk_0513"><i class="fa fa-twitter-square fa-3x faa-spin animated-hover"></i></a> 
+			<a href="https://github.com/mjk0513"><i class="fa fa-github fa-3x faa-spin animated-hover"></i></a>
 
 		</div>
 
